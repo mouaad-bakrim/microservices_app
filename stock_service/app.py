0a22219a7a6400666b36fbb55fba8c40ff26b9ea
mouaad-bakrim <bakrim2mo@gmail.com>
2025-10-22 17:32:37 +0100

    Initial commit : microservices architecture vente/achat/stock/facture

---
+++ stock_service/app.py
@@ -0,0 +1,29 @@
+from flask import Flask, jsonify, request
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///stock.db'
+db = SQLAlchemy(app)
+
+class Produit(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nom = db.Column(db.String(100))
+    quantite = db.Column(db.Integer)
+    prix_unitaire = db.Column(db.Float)
+
+@app.route('/produits', methods=['GET'])
+def get_produits():
+    produits = Produit.query.all()
+    return jsonify([{"id": p.id, "nom": p.nom, "quantite": p.quantite, "prix": p.prix_unitaire} for p in produits])
+
+@app.route('/update_stock', methods=['POST'])
+def update_stock():
+    data = request.json
+    produit = Produit.query.get(data['id'])
+    produit.quantite -= data['quantite']
+    db.session.commit()
+    return jsonify({"message": "Stock mis à jour"})
+
+if __name__ == '__main__':
+    db.create_all()
+    app.run(host='0.0.0.0', port=5001)
